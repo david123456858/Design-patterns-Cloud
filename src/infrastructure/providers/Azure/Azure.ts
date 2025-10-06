4dd964cfbd7b4a7fce76c30114e3a1349aee4bba
david123456858 <juandavidperaltafuentes@gmail.com>
2025-10-06 10:03:21 -0500

    todo hecho

--- src/infrastructure/providers/Azure/Azure.ts
+++ src/infrastructure/providers/Azure/Azure.ts
@@ -1,22 +1,49 @@
-
-import { ProvisionRequestDTO } from '../../../api/dto/VM/provisioning'
+import { AzureVMDTO } from '../../../api/dto/VM'
 import { StatusResult } from '../../../common/enums/StatusResult'
 import { Cloud } from '../../../domain/entities/Cloud'
-import { Disk } from '../../../domain/entities/Disk'
-import { Network } from '../../../domain/entities/Network'
-
 import { ProvisionResult } from '../../../domain/valueObjects/ProvisionResult'
+import { AzureDisk } from './Disk/AzureDisk'
+import { AzureNetwork } from './Network/AzureNetwork'
+import { Azurevm } from './VirtualMachine/AzureVm'
 
 export class AZURE extends Cloud {
-  public async diskSupply (disk: Disk): Promise<ProvisionResult> {
-    throw new Error('Method not implemented.')
+  public async diskSupply (disk: any): Promise<ProvisionResult> {
+    const diskInstance = new AzureDisk(
+      disk.idDisk,
+      disk.scale,
+      disk.sizeGB,
+      disk.type,
+      disk.autoDelete
+    )
+    return new ProvisionResult(StatusResult.SUCCESS, diskInstance, 'Disk Created')
   }
 
-  public async netWorkSupply (net: Network): Promise<ProvisionResult> {
-    throw new Error('Method not implemented.')
+  public async netWorkSupply (net: any): Promise<ProvisionResult> {
+    const networkInstance = new AzureNetwork(
+      net.idNetwork,
+      net.securityPolicy,
+      net.virtualNetwork,
+      net.subnetName,
+      net.networkSecurityGroup
+    )
+    return new ProvisionResult(StatusResult.SUCCESS, networkInstance, 'Network Created')
   }
 
-  public async vmSupply (vm: ProvisionRequestDTO): Promise<ProvisionResult> {
-    return new ProvisionResult(StatusResult.SUCCESS, vm, 'In Deployment...')
+  public async vmSupply (vm: AzureVMDTO): Promise<ProvisionResult> {
+    // Crea las entidades de dominio desde el DTO
+    const net = await this.netWorkSupply(vm.network)
+    const disk = await this.diskSupply(vm.disk)
+
+    const vmInstance = new Azurevm(
+      '32GB',
+      '8',
+      vm.type,
+      vm.resourceGroup,
+      vm.redVital,
+      net.getObject(),
+      disk.getObject()
+    )
+
+    return new ProvisionResult(StatusResult.SUCCESS, vmInstance, 'Azure VM in Deployment...')
   }
 }
--- src/infrastructure/providers/Azure/VirtualMachine/AzureVm.ts
+++ src/infrastructure/providers/Azure/VirtualMachine/AzureVm.ts
@@ -1,15 +1,30 @@
 import { AzureMachineType } from '../../../../common/enums/MachineTypes'
 import { VM } from '../../../../domain/entities/VM'
+import { AzureDisk } from '../Disk/AzureDisk'
+import { AzureNetwork } from '../Network/AzureNetwork'
 
 export class Azurevm extends VM {
+  private _type: AzureMachineType
+  private _resourceGroup: string
+  private _redVital: string
+  private readonly network: AzureNetwork
+  private readonly disk: AzureDisk
+
   constructor (
     ram: string,
     cpus: string,
-    private _type: AzureMachineType,
-    private _resourceGroup: string,
-    private _redVital: string
+    type: AzureMachineType,
+    resourceGroup: string,
+    redVital: string,
+    network: AzureNetwork,
+    disk: AzureDisk
   ) {
     super(ram, cpus)
+    this._type = type
+    this._resourceGroup = resourceGroup
+    this._redVital = redVital
+    this.network = network
+    this.disk = disk
   }
 
   // Getter y Setter de type
@@ -37,5 +52,14 @@ export class Azurevm extends VM {
 
   public set redVital (value: string) {
     this._redVital = value
+  }
+
+  // Getters para network y disk
+  public getNetwork (): AzureNetwork {
+    return this.network
+  }
+
+  public getDisk (): AzureDisk {
+    return this.disk
   }
 }
